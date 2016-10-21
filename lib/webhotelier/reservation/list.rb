d02d0bdc69ad0d7844a2eaf460cfb4654c369e74
Maxim Pechnikov <parallel588@gmail.com>
2016-10-21 08:37:54 +0300

    fix list resource

--- lib/webhotelier/reservation/list.rb
+++ lib/webhotelier/reservation/list.rb
@@ -1,17 +1,24 @@
 module Webhotelier
   module Reservation
-
     class List
       include Enumerable
-      class Entry < Struct.new(:id, :property, :status); end
+      Entry = Struct.new(:id, :property, :status, :resource) do
 
-      attr_reader :data
+      end
 
-      def initialize(data)
-        if data
-          @data = data['reservations'].map { |j|
-            Entry.new(j['id'], j['property'], j['status'])
-          }
+      attr_reader :data, :raw_data
+
+      def initialize(raw_data = nil)
+        @raw_data = raw_data
+        if raw_data
+          @data = Array(raw_data['reservations']).lazy.map do |j|
+            Entry.new(
+              j['id'],
+              j['property'],
+              j['status'],
+              Resource.new(HashConverter.to_underscore(j))
+            )
+          end
         else
           @data = []
         end
